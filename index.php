8b889420b8f078da286e85a64d5d528265d67828
jonathanbe <jonathan.on@hotmail.com>
2015-11-15 20:37:03 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,535 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>bryvo studios - Web Design In Ottawa!</title>
+		<meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">    
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+
+<!--<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"> -->       
+<link rel="icon" type="image/png" sizes="96x96" href="favicon2.png">    
+	  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">	
+<link href='https://fonts.googleapis.com/css?family=Roboto:400,300,500,700,100' rel='stylesheet' type='text/css'><link href='https://fonts.googleapis.com/css?family=Cousine:400,700' rel='stylesheet' type='text/css'>		
+<link href='https://fonts.googleapis.com/css?family=Quicksand:400,700,300' rel='stylesheet' type='text/css'>   
+<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,300,700' rel='stylesheet' type='text/css'>	
+    <link rel="stylesheet" href="/contact/general.css" type="text/css" media="screen" />
+	<script src="http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js" type="text/javascript"></script>
+	<script src="/contact/popup.js" type="text/javascript"></script>    
+        
+  <link rel="stylesheet" href="wow/css/libs/animate.css">
+  <link rel="stylesheet" href="wow/css/site.css">
+		
+        
+		<link rel="stylesheet" href="css/style.css" />
+		<link rel="stylesheet" href="css/search.css" />
+		<link rel="stylesheet" href="css/pricing.css" />
+		<link rel="stylesheet" href="css/animate.css" />
+		<link rel="stylesheet" href="css/main.css" />
+      <link rel="stylesheet" href="new.css" />
+	
+        
+ 
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script>
+	$(function() {
+	  $('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	  });
+	});
+	</script>
+ 	
+        <script src="js/wow.min.js"></script>
+              <script>
+              new WOW().init();
+              </script>
+	</head>
+	<body><!--
+        <div class="animated fadeInDown"> 
+        <div class="motto">Simple. Clean. Elegeant.</div>    
+        </div>-->
+        
+		<!-- Page Wrapper -->
+		<div id="page-wrapper">
+
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="index.html" style="letter-spacing:2px;"></a></h1>
+						<nav id="nav">
+							<ul>
+								<li class="special">
+									<a href="#" class="menuToggle"><span>Menu <font size="3pt;"><i class="fa fa-bars"></i></font></span></a>
+									<div id="menu">
+										<ul>
+											<li><a href="#aboutus">About</a></li>
+											<li><a href="#choosingus">Choosing Us</a></li>
+											<li><a href="#howwecanhelp">Our Skillset</a></li>
+                                            <li><a href="#ourportfolio">Portfolio</a></li>
+											<li><a href="#pricinglane">Pricing</a></li>
+											<li><a href="#contactsection">Contact Us</a></li>
+											
+										</ul>
+									</div>
+								</li>
+							</ul>
+						</nav>
+					</header>
+            <div class="frontery">
+
+<section id="banner">
+    
+
+   <div class="frontery2"><br>
+<center>
+       <div class="animated fadeInDown"> 
+            <center><img src="images/bryvos.png" /></center>
+    </div> <!---- fadeindown ----->
+    
+    <div class="animated fadeInUp">  
+    <h1>a web design studio in ottawa.</h1>
+    
+  <h1>let's make something nice</h1>
+    </div><!----- fadeindown ------->
+    <br><br>
+       </center>
+      
+    <div id="animated-example" class="animatedbutton fadeInLeft">
+    <div class="buttons">
+    <center>
+    <a class="aboutme" href="#aboutus">about us</a>&nbsp;&nbsp;&nbsp;
+    <a class="firstbu" href="#ourportfolio">browse work</a>
+    </center>
+    </div> <!----- buttons ----->
+    </div> <!----- buttons animation ---->
+    </div>  </section>        
+            </div>        
+            </div>        
+        
+        
+<div class="continued">        
+<div class="mbgw1">
+<div class="spacer">
+<div class="spacer">
+    <h1 id="aboutus"></h1><br><br><br><br>
+
+   <br> <h1 style="text-align:center;font-weight:100;">About Us</h1>
+    <center><img src="images/h1-bg.png" /></center><br><br>
+We are a team of UX/UI designers &amp; front-end developers residing in Ottawa, Canada. We have spent the last few years of our lives sitting in front of monitors making websites. It started out as a something we did to challenge ourselves, but then it became a hobby, and we are happy to say that this is our job.
+   
+<br><br>
+    
+We want to give our customers and clients what they want. If you can think about an idea, we can bring it to life. <br><br>
+We provide an assortment of different web services. So far, we have created social networks for schools, travel websites, countless landing pages, and a large variety of websites for small businessed. We also provide redesigns, testing, and website setup services.     
+
+<br><br>
+<br><br>
+  <br><br>
+    </div> <!----- spacer ---->
+    </div> <!----- spacer ---->
+        </div><!---mbgw---->
+    
+<div class="proj">
+<div class="spacer">
+    "There are three responses to a piece of design – yes, no, and WOW! Wow is the one to aim for.”
+    <br><i>-Milton Glaser</i>
+    </div>
+</div>    
+    
+<div class="mbgw">
+<div class="spacer">   
+    <br>
+    <center> <h1 id="choosingus"></h1><br><br><br><br>
+  <h1 class="pickus">Why Choose Us?</h1><br>
+        <br><br>
+    </center>
+    
+    <div class="spacer">
+    <div class="spacer">
+        <section class="wow fadeInLeft" data-wow-offset="100" style="height:auto;">
+    <h1 class="pickus">Experience</h1><br>
+    
+            We are a team of very skilled web designers &amp; developers with an average of 6 years of experience in this field. We want to give you the exact website you want. We work closely with our clients to deliver modern design, and to help provide insight on the best possible solutions for their web services.<br><br><br>
+        
+    <h1 class="pickus">Pricing</h1><br>
+         We have fair and reasonable pricing options.<a class="pricing" href="">&nbsp;View our pricing options&nbsp;</a> We don't have any hidden fees. We ask for 1/3 when we start, 1/3 on your design approval, and 1/3 upon completion. <br><br><br>
+        </section>
+       
+        
+        
+          <section class="wow fadeInRight" data-wow-offset="100" style="height:auto;">
+        <h1 class="pickus">Do You Need A Website?</h1><br>
+        Probably. If you have a business, or want to start one, then absolutely. We live in a time where poeple don't go a day without looking at screens. People use laptops &amp; computers, snartphones &amp; tablets, and now even smart TVs to browse some sort of web service every single day, so it is important to have your presence online. WIth your own website, you have complete control of what your services are to your customers. <br><br>
+        If you only have asocial media page, you will always be limited with what information you can provide. Having your own website (and email accounts) also looks more professional, plain and simple.
+        <br><br><br>
+   
+        
+        <h1 class="pickus">Work Ethic</h1><br>
+        Each and every website we make starts from nothing. We don't use any templates, or any drag and drop programs. We write code. Some wbesites allow you to have drag and drop content on a blank page; and that's not a bad solution, but it's not the best. <br><br>Think about buying a pizza. A drag and drop website would be like buying a frozen pizza and putting it in a microwave for five minutes. It's easy, but not always great. Writing code is like baking a pizza using fresh ingredients; the timing is similar, but the outcome is much better. <br><br><br>
+        </section>
+        
+          <section class="wow fadeInLeft" data-wow-offset="100" style="height:auto;">
+        <h1 class="pickus">Website Setup</h1><br>
+        Once you have the code for your wbesite, there are still a few things you will need to do. You will need a domain name and hosting, as well as uploading the actual site itself. We will help you setup your website for a small fee iff you need it.<br><a class="pricing" href="">&nbsp;View our setup fees&nbsp;</a> <br><br>
+        <strong style="color:#222;font-weight:bold;">Note:</strong> This isn't a requirement with us. If you want to figure it out on your own, go for it!
+              <br> <br><br><br><br></section>
+    </div></div><!--- spacer--->
+    
+</div> <!---- spacer --->
+    
+ 
+<div class="resources">
+<h1 id="howwecanhelp"></h1><br><br><br><br>        
+<div class="resinfo">    
+ <h1>How We Can Help</h1>  <br><br>
+<center>
+<table>
+<tr>
+<td>Web Design</td> 
+<td>Graphic Design</td>
+<td>Web Hosting</td>    
+</tr>    
+
+<tr>
+<td>Responsive Design</td> 
+<td>Website Setup</td>
+<td>Logo Creation</td>    
+</tr>      
+  
+<tr>
+<td>Social Media Setup</td> 
+<td>Wordpress Installation</td>
+<td>eCommerce Solutions</td>    
+</tr>      
+    
+<tr>
+<td>Website Redesign</td> 
+<td>Animations</td>
+<td></td>    
+</tr>      
+    
+    
+</table>
+<br><br><br>    
+
+<img src="images/html5.png" style="width:65px;height:65px;" />    
+&nbsp;&nbsp;&nbsp;    
+<img src="images/php.png" style="width:85px;height:65px;" />    
+&nbsp;&nbsp;&nbsp;    
+<img src="images/css3.png" style="width:85px;height:65px;" />       
+&nbsp;&nbsp;&nbsp;    
+<img src="images/wp.png" style="width:65px;height:65px;" />    
+&nbsp;&nbsp;&nbsp;    
+<img src="images/ps.png" style="width:65px;height:65px;" />        
+    
+</center>
+</div>   
+    </div>    
+    
+    
+
+    <h1 id="ourportfolio"></h1><br><br><br><br>
+     <h1 class="port">[Portfolio]</h1><br><br>
+    
+    <div class="spacer5">
+    
+   Our latest work includes: <br>Help with the redesign of Schoolitup, backend work on vacationdeals4all, RM Construction, a demo for a school project called AudiCross, and Shoppery2's landing page design.<br><br>We also designed this site, but you probably assumed that...<br><br>
+        <div class="remmob">Click the links to see the sites in action!</div>
+    </div>  <br><br><br><br>
+    </div><!---mkbv--->
+ 
+    <div class="port1">
+
+    </div>
+    
+    
+    
+<div class="mbgw">
+    <br><br>
+    <br><br>
+ 
+<div class="mbgw">
+    
+ 
+    
+   <!-- <h1 class="pickus" style="text-align:center;">Our Mission</h1>-->
+   
+  <!--      
+  <div class="portrow1" >  
+    
+<div id="kyastoportfolio">
+<div class="kyastoportfolio"></div>
+</div> 
+    
+<div id="schoolitupportfolio">
+<div class="schoolitupportfolio"></div>
+</div>
+ 
+    </div> 
+        
+
+        -->
+    <div class="remmob">
+  
+
+<center>
+    <table class="portfolio">
+        
+<tr>
+
+<td class="portfolioitems">
+    
+ <div id="schoolitupportfolio">
+  <div class="schoolitupportfolio"></div>
+ </div>
+    
+</td>
+        
+<td class="portfolioitems2">
+<p class="port2"><a class="portfoliotext" href="http://www.schoolitup.com/" target="_blank">Schoolitup</a></p>
+  </td>        
+        
+</tr>
+        
+  
+        
+<tr>      
+<td class="portfolioitems2">
+<p class="port2"><a class="portfoliotext" href="http://jonathanbe.github.io/kyasto" target="_blank">Kyasto Kicks</a></p>
+  </td>        
+    
+<td class="portfolioitems">
+<div id="kyastoportfolio">
+<div class="kyastoportfolio"></div>
+</div>     
+</td>
+    
+</tr>
+     
+        
+<tr>        
+ <td class="portfolioitems">
+    
+ <div id="schoolitup_photos_portfolio">
+  <div class="schoolitup_photos_portfolio"></div>
+ </div>
+    
+</td>
+        
+<td class="portfolioitems2">
+<p class="port2"><a class="portfoliotext" href="http://www.schoolitup.com/photos" target="_blank">Schoolitup [Photos]</a></p>
+  </td>        
+        
+</tr>     
+        
+        
+   
+        
+        
+  
+        
+        
+<tr>      
+<td class="portfolioitems2">
+<p class="port2"><a class="portfoliotext" href="http://www.vacationdeals4all.com/" target="_blank">VacationDeals4All</a></p>
+  </td>        
+    
+<td class="portfolioitems">
+<div id="vd4aportfolio">
+<div class="vd4aportfolio"></div>
+</div>     
+</td>
+    
+</tr>        
+        
+        
+<tr>
+
+<td class="portfolioitems">
+    
+ <div id="rmcportfolio">
+  <div class="rmcportfolio"></div>
+ </div>
+    
+</td>
+        
+<td class="portfolioitems2">
+    <p class="port2">RMConstruction</p>
+  </td>        
+        
+</tr>        
+     
+        
+        
+        
+        
+<tr>      
+<td class="portfolioitems2">
+<p class="port2"><a class="portfoliotext" href="http://www.vacationdeals4all.com/" target="_blank">VacationDeals4All [Booking]</a></p>
+  </td>        
+    
+<td class="portfolioitems">
+<div id="vd4a_booking_portfolio">
+<div class="vd4a_booking_portfolio"></div>
+</div>     
+</td>
+    
+</tr>             
+        
+        
+    </table>
+    </center> 
+</div>    
+</div>    
+  <!--  <center>
+  <h1 id="choosingus"></h1><br><br><br><br>
+  <h1 class="pickus">Our Mission</h1><br>  
+    <img src="images/mission.png" style="width:400px;margin-top:-240px;"/>
+    <div class="spacer5" style="text-align:left;">
+    
+   Our latest work includes: <br>Help with the redesign of Schoolitup, backend work on vacationdeals4all, RM Construction, a demo for a school project called AudiCross, and Shoppery2's landing page design.<br><br>We also designed this site, but you probably assumed that...
+    </div>   
+    </center>
+    
+    -->
+
+    
+    </div>    
+    
+  <!---- whitespace.html goes here ---->
+        
+    
+   <div class="contactbottom">
+       <h1 id="contactsection"></h1><br><br><br><br>
+   <br><br>   <center style="font-size:16pt !important;">Have a question? Ask us anything! <br><br><br>
+       
+       Our support team will do our best to respond within 24 hours.<br><br><br>
+        <i class="fa fa-map-marker"></i>  Ottawa/Kanata, ON, Canada <br><br>
+       
+       <i class="fa fa-envelope-o"></i>  info@bryvostudios.com<br><br></center>
+   <br>  
+        <center>	
+		<div id="button" style="padding:0 autp;margin:0 auto;">
+            
+<input type="submit" class="contactbutton" value="Contact Us"/></div>
+	</center><br>
+	<div id="popupContact">
+		<a id="popupContactClose" style="font-weight:bold;color:#ff406d;font-size:24pt;text-transform:lowercase;letter-spacing:-4px;padding-right:10px;">--</a>
+			<script>
+	$(function() {
+	  $('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	  });
+	});
+	</script>
+
+
+<?php 
+$action=$_REQUEST['action']; 
+if ($action=="")    /* display the contact form */ 
+    { 
+    ?>
+  
+ <br><br>
+    <form  action="" method="POST" enctype="multipart/form-data"> 
+    <input type="hidden" name="action" value="submit"> 
+    <input class="caero" name="name" type="text" placeholder="Name or Phone Number" value="" size="30"/><br> 
+    <input class="caero" name="email" type="text" placeholder="Your Email" value="" size="30"/><br> 
+    <textarea class="caero" name="message" placeholder="Leave us a Message" rows="5" cols="30"></textarea><br>
+    <input type="submit" class="button" style="width:95%;outline:none;"value="Email Us!"/><br><br>
+
+    </form> <br>
+    
+    <?php 
+    }  
+else                /* send the submitted data */ 
+    { 
+    $name=$_REQUEST['name']; 
+    $email=$_REQUEST['email']; 
+    $message=$_REQUEST['message']; 
+    if (($name=="")||($email=="")||($message=="")) 
+        { 
+        echo "All fields are required, please fill <a href=\"\"> the form </a> again."; 
+        } 
+    else{         
+        $from="From: $name<$email>\r\nReturn-path: $email"; 
+        $subject="Message sent using your contact form"; 
+        mail("Quity1992@cuvox.de", $subject, $message, $from); 
+        echo "Email sent!"; 
+        } 
+    }   
+?> 
+
+	</div>
+	<div id="backgroundPopup"></div>
+    
+       <!--- oldcontact bebeh --->
+ 
+   
+    </div><!--contactbottom--->
+      
+
+ 
+
+       
+
+			<script src="js/jquery.min.js"></script>
+			<script src="js/jquery.scrollex.min.js"></script>
+			<script src="js/jquery.scrolly.min.js"></script>
+			<script src="js/skel.min.js"></script>
+			<script src="js/init.js"></script>
+      <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+        
+        
+<<<<<<< HEAD
+<<<<<<< HEAD
+         <script src="/wow/dist/wow.min.js"></script>
+=======
+         <script src="/bryvo-studios/wow/dist/wow.min.js"></script>
+>>>>>>> origin/gh-pages
+=======
+         <script src="/bryvo-studios/wow/dist/wow.min.js"></script>
+>>>>>>> origin/gh-pages
+  <script>
+    wow = new WOW(
+      {
+        animateClass: 'animated',
+        offset:       100,
+        callback:     function(box) {
+          console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+        }
+      }
+    );
+    wow.init();
+    document.getElementById('moar').onclick = function() {
+      var section = document.createElement('section');
+      section.className = 'section--purple wow fadeInDown';
+      this.parentNode.insertBefore(section, this);
+    };
+  </script>
+        
+        </div>
+    </body>
+    </html>
+   
